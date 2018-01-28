f362990e92eeb6bfa7ea5151c03754172d55f29c
Eden Mesfun <emesfun@gmail.com>
2018-01-28 22:11:34 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -33,7 +33,7 @@ def sum_array(array)
 end
 
 def add_s(array) #work on
-  array.each_with_index.collect { |element, index| element+'s' }
+  array.each_with_index.collect { |element, index| element[2,3]+'s' }
 end
 
   # [1,2].each_with_index.collect{|element, index|}
